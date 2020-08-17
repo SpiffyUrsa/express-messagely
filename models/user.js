b4198e6b3df1953161098386634d8f69bd17fdcb
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-08-17 15:53:17 -0700

    Created User class methods and edited the default value of the last_login_at table.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 "use strict";
-
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
 /** User of the site. */
 
 class User {
@@ -9,22 +12,65 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+
+    let hashPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(`
+      INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone, join_at
+    `, [username, hashPwd, first_name, last_name, phone]);
+    
+    const user = result.rows[0];
+    return user;
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+
+    const result = await db.query(`
+      SELECT password 
+      FROM users 
+      WHERE username = $1 
+    `, [username]);
+
+    const userData = result.rows[0];
+    
+    return await bcrypt.compare(password, userData.password) === true;
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+
+    const result = await db.query(`
+      UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1
+      RETURNING username, last_login_at
+      `, [username]);
+
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`The user ${username} does not exist.`);
+
+    return user;
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+
+    const result = await db.query(`
+      SELECT username, first_name, last_name
+      FROM users
+    `);
+
+    const allUsers = result.rows;
+
+    return allUsers;
   }
 
   /** Get: get user by username
@@ -37,6 +83,18 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+
+    const result = await db.query(`
+      SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users
+      WHERE username = $1
+    `, [username]);
+
+    let user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`The user ${username} does not exist.`);
+
+    return user;
   }
 
   /** Return messages from this user.
@@ -48,6 +106,37 @@ class User {
    */
 
   static async messagesFrom(username) {
+    
+    const results = await db.query(`
+        SELECT m.id, 
+              m.body, 
+              m.sent_at, 
+              m.read_at, 
+              m.to_username, 
+              u.username, 
+              u.first_name, 
+              u.last_name, 
+              u.phone
+        FROM messages AS m
+        JOIN users AS u ON m.to_username = u.username
+        WHERE m.from_username = $1
+      `, [username]);
+
+    let msgsFromUser = results.rows;
+
+    return msgsFromUser.map(msg => ({
+      id: msg.id,
+      to_user: {
+        username: msg.username,
+        first_name: msg.first_name,
+        last_name: msg.last_name,
+        phone: msg.phone
+      },
+      body: msg.body,
+      sent_at: msg.sent_at,
+      read_at: msg.read_at,
+    }));
+
   }
 
   /** Return messages to this user.
@@ -55,12 +144,44 @@ class User {
    * [{id, from_user, body, sent_at, read_at}]
    *
    * where from_user is
-   *   {id, first_name, last_name, phone}
+   *   {username, first_name, last_name, phone}
    */
 
   static async messagesTo(username) {
+
+    const results = await db.query(`
+      SELECT m.id, 
+            m.from_username,
+            u.username,  
+            u.first_name, 
+            u.last_name, 
+            u.phone, 
+            m.body, 
+            m.sent_at, 
+            m.read_at
+      FROM messages AS m 
+      JOIN users AS u ON m.from_username = u.username
+      WHERE m.to_username = $1
+    `, [username]);
+
+    let msgsToUser = results.rows;
+
+    return msgsToUser.map(msg => ({
+      id: msg.id,
+      from_user: {
+        username: msg.username,
+        first_name: msg.first_name,
+        last_name: msg.last_name,
+        phone: msg.phone
+      },
+      body: msg.body,
+      sent_at: msg.sent_at,
+      read_at: msg.read_at,
+    }));
   }
 }
 
+// PLUGIN that allows you to select two blocks of text and show differences. CHECK IT OUT
+// ^ SIDEBYSIDE DIFF or something like that
 
 module.exports = User;
